abd97a50708900434670b8078d30e8a06ef60a4d
navdeep <navdeepsg04@gmail.com>
2025-11-10 16:04:00 -0500

    very barebones idea for the table entity

---
+++ src/main/java/entity/DataRow.java
@@ -0,0 +1,11 @@
+package entity;
+
+import java.util.List;
+
+public class DataRow {
+    private final List<String> cells;
+
+    public DataRow(List<String> cells) {
+        this.cells = cells;
+    }
+}
---
+++ src/main/java/entity/DataSet.java
@@ -0,0 +1,11 @@
+package entity;
+
+import java.util.List;
+
+public class DataSet {
+    private final List<DataRow> rows;
+
+    public DataSet(List<DataRow> rows) {
+        this.rows = rows;
+    }
+}

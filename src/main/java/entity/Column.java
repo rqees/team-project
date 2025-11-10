1d6117d595e68fcf5ce93a02fa802c638dc45cf8
navdeep <navdeepsg04@gmail.com>
2025-11-10 16:32:20 -0500

    added columns to the Dataset implementation

---
+++ src/main/java/entity/Column.java
@@ -0,0 +1,11 @@
+package entity;
+
+import java.util.List;
+
+public class Column {
+    private final List<String> cells;
+
+    public Column(List<String> cells) {
+        this.cells = cells;
+    }
+}
--- src/main/java/entity/DataSet.java
+++ src/main/java/entity/DataSet.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public class DataSet {
     private final List<DataRow> rows;
+    private final List<Column> columns;
 
-    public DataSet(List<DataRow> rows) {
+    public DataSet(List<DataRow> rows,  List<Column> columns) {
         this.rows = rows;
+        this.columns = columns;
     }
 }

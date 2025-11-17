05ea2aab09120f65cd3739a4a8e2e3c797fd48e9
Dohyeon <dohyeon.nam@mail.utoronto.ca>
2025-11-17 00:15:24 -0500

    basic clean

---
+++ src/main/java/usecase/DataCleaner.java
@@ -0,0 +1,59 @@
+package usecase;
+
+import entity.DataSet;
+import entity.DataRow;
+import entity.Column;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SimpleTimeZone;
+
+public class DataCleaner {
+    private final DataSet dataSet;
+
+    public DataCleaner(DataSet dataSet) {
+        this.dataSet = dataSet;
+    }
+
+    // edit a single cell
+    public void editCell(int rowIndex, int colIndex, String newValue) {
+        dataSet.setCell(newValue, rowIndex, colIndex);
+    }
+
+
+    // find missing cells
+    public List<CellLocation> findMissingCells() {
+        List<CellLocation> missingCells = new ArrayList<>();
+        List<DataRow> rows = dataSet.getRows();
+        List<String> cells;
+
+        for(int i = 0; i < rows.size(); i++) {
+            DataRow row = rows.get(i);
+            cells = row.getCells();
+            for(int j = 0; j < cells.size(); j++) {
+                String value = cells.get(j);
+                if(isMissing(value)) {
+                    missingCells.add(new CellLocation(i, j));
+                }
+            }
+        }
+
+        return missingCells;
+    }
+
+    // helper to identify missing cells
+    private boolean isMissing(String s) {
+        return s == null || s.isBlank();
+    }
+
+    // helper class to identify a cell
+    public static class CellLocation {
+        public final int row;
+        public final int col;
+
+        public CellLocation(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+}

d59e00315e7ab46c0427ddaa0450e273eeadcb19
navdeep <navdeepsg04@gmail.com>
2025-11-17 16:20:21 -0500

    added datatype as an attribute for the columns and made the setCell protected so it cant be called on its own

--- src/main/java/entity/Column.java
+++ src/main/java/entity/Column.java
@@ -4,16 +4,22 @@ import java.util.List;
 
 public class Column {
     private final List<String> cells;
+    private final Object datatype;
 
-    public Column(List<String> cells) {
+    public Column(List<String> cells, Object datatype) {
         this.cells = cells;
+        this.datatype = datatype;
     }
 
     public List<String> getCells() {
         return cells;
     }
 
-    public void setCell(String value, int index) {
+    public Object getDataType() {
+        return datatype;
+    }
+
+    protected void setCell(String value, int index) {
         this.cells.set(index, value);
     }
 }

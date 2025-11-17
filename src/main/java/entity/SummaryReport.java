dd31d195465a95db0030ce21db0d652e870bb150
Delfat <fatshinli@gmail.com>
2025-11-17 16:33:30 -0500

    Proposed entities for Summary Statistics small changes

--- src/main/java/entity/SummaryReport.java
+++ src/main/java/entity/SummaryReport.java
@@ -1,13 +1,15 @@
 package entity;
 
+import java.util.List;
+
 public class SummaryReport {
     private final int summary_id;
     private final String report_name;
     private final DataSet dataset;
-    private final Column columns_used;
+    private final List<Column> columns_used;
     private final DataRow data_rows_used;
 
-    public SummaryReport(int summary_id, String report_name, Column columns_used, DataRow data_rows_used, DataSet dataset) {
+    public SummaryReport(int summary_id, String report_name, List<Column> columns_used, DataRow data_rows_used, DataSet dataset) {
         this.summary_id = summary_id;
         this.report_name = report_name;
         this.columns_used = columns_used;

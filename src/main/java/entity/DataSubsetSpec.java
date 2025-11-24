30a26ab9d60edb8a5e44af718aa18bfa60a55860
rqees <kabir.raees@gmail.com>
2025-11-23 19:32:12 -0500

    add DataSubsetSpec entity

---
+++ src/main/java/entity/DataSubsetSpec.java
@@ -0,0 +1,29 @@
+package entity;
+
+import java.util.List;
+
+public class DataSubsetSpec {
+    private final String datasetId;
+    private final List<String> columnNames;
+    private final List<Integer> rowIndices;
+
+    public DataSubsetSpec(String datasetId, List<String> columnNames, List<Integer> rowIndices) {
+        this.datasetId = datasetId;
+        this.columnNames = columnNames;
+        this.rowIndices = rowIndices;
+    }
+
+    public String getDatasetId() {
+        return datasetId;
+    }
+
+    public List<String> getColumnNames() {
+        return columnNames;
+    }
+
+    public List<Integer> getRowIndices() {
+        return rowIndices;
+    }
+
+
+}
